19b41e53deb2fcdc407e4240750536c0b6e85735
sham-suresh-s <165762750+sham-suresh-s@users.noreply.github.com>
2024-09-04 18:17:40 +0530

    Final Commit

--- src/main/java/com/sm/CustomerController.java
+++ src/main/java/com/sm/CustomerController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
@@ -18,13 +19,28 @@ public class CustomerController {
     public List<Customer> getAllCustomers() {
         return customerService.getAllCustomers();
     }
-
+    
     @GetMapping("/{id}")
     public ResponseEntity<Customer> getCustomerById(@PathVariable int id) {
         Optional<Customer> customer = customerService.getCustomerById(id);
         return customer.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
+    @PutMapping("/{id}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable int id, @RequestBody Customer customerDetails) {
+        Optional<Customer> optionalCustomer = customerService.getCustomerById(id);
+        if (optionalCustomer.isPresent()) {
+            Customer customer = optionalCustomer.get();
+            customer.setName(customerDetails.getName());
+            customer.setEmail(customerDetails.getEmail());
+            customer.setPhoneNumber(customerDetails.getPhoneNumber());
+            customer.setAddress(customerDetails.getAddress());
 
+            Customer updatedCustomer = customerService.saveCustomer(customer);
+            return ResponseEntity.ok(updatedCustomer);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
     @PostMapping
     public Customer createCustomer(@RequestBody Customer customer) {
         return customerService.saveCustomer(customer);
--- src/main/java/com/sm/CustomerRepository.java
+++ src/main/java/com/sm/CustomerRepository.java
@@ -3,5 +3,6 @@ package com.sm;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+	
 }
 
